98068b67e36214cedbd065473e7ec1f83edbd751
Gabriel Toro <gatoroguzman@gmail.com>
2022-04-10 20:41:50 -0400

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Gabriel Toro on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
